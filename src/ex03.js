238f8875938fdc3415e5523ce4954fa90ba084fd
Sae Nakatani <nakatanisae0218@gmail.com>
2026-06-11 12:22:39 -0700

    In progress

--- src/ex03.js
+++ src/ex03.js
@@ -4,13 +4,25 @@
  * @returns {number}
  */
 function calculateFactorial(number) {
-  let result = 1;
+  if(number < 0){
+    return 'Number cannot be negative'
+  } else if(number === 'string') {
+    return 'Argument must be a number'
+  } else if(number === ''){
+    return 'Number cannot be undefined'
+  } else if(number === number.round){
+    return 'Number must be an integer'
+  } else {
+      let result = 1;
 
-  for (let current = 1; current <= number; current += 1) {
-    result *= current;
+      for (let current = 1; current <= number; current += 1) {
+        result *= current;
+      }
+
+      return result;
   }
 
-  return result;
+
 }
 
 module.exports = calculateFactorial;
