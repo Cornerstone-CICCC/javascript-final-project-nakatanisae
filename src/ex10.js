af9a073152830ec25ba4f6fbc299656344629852
Sae Nakatani <nakatanisae0218@gmail.com>
2026-06-11 12:27:10 -0700

    In progress

--- src/ex10.js
+++ src/ex10.js
@@ -4,6 +4,14 @@
  * @returns {{itemCount: number, totalQuantity: number, totalPrice: number}}
  */
 function createCartSummary(items) {
+  if(typeof price !== 'number' || typeof quantity !== 'number'){
+    return 'Price and quantity must be numbers'
+  } else if(price < 0 || quantity < 0){
+    return 'Price and quantity cannot be negative'
+  }
+    
+  
+
   return {
     itemCount: items.length,
     totalQuantity: items.reduce((total, item) => total + item.quantity, 0),
