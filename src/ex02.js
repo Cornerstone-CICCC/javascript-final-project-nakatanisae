1245eac8af020634781f20b8b06709e8bfb5095d
Sae Nakatani <nakatanisae0218@gmail.com>
2026-06-11 13:46:42 -0700

    Exercise 2 completed

--- src/ex02.js
+++ src/ex02.js
@@ -4,13 +4,17 @@
  * @returns {number}
  */
 function sumArray(arr) {
-    if(arr === 'undifined'){
-      return 'Array cannot be undefined'
+    if(arr === undefined){
+      throw new Error('Array cannot be undefined')
+    } else if(!Array.isArray(arr)){
+      throw new Error('Argument must be an array')
     } else {
-      return arr.reduce((total, number) => total + number, 0);
+      return arr.reduce((total, number) => {
+        if(typeof number !== 'number'){
+          throw new Error('Array can only contain numbers')
+        }
+       return total + number}, 0);
     }
-    
-  
   
 }
 
