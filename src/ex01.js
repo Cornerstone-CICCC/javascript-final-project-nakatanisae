4efe6d0d9e8998514f175d23f0d821d93bc4ce23
Sae Nakatani <nakatanisae0218@gmail.com>
2026-06-11 12:22:09 -0700

    In progress

--- src/ex01.js
+++ src/ex01.js
@@ -4,6 +4,17 @@
  * @returns {number}
  */
 function findMax(arr) {
+  let largestNum = arr[i]
+  const secondLargestNum = arr[i + 1]
+  for(let i = 0; i <= arr.length; i++)
+    if(largestNum > secondLargestNum){
+      return largestNum
+    } else {
+      return largestNum = secondLargestNum
+    }
+      
+    
+
   if (arr.length <= 0) throw new Error('Array cannot be empty');
 
   return arr[0];
