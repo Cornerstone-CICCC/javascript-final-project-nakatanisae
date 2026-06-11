9f17a1e411649941a6ddf98755ff4139af82ff53
Sae Nakatani <nakatanisae0218@gmail.com>
2026-06-11 14:29:31 -0700

    Exercise 5 completed

--- src/ex05.js
+++ src/ex05.js
@@ -4,12 +4,17 @@
  * @returns {number[]}
  */
 function filterEvenNumbers(arr) {
-  if(arr.includes('')){
-    return 'Array cannot be undefined'
-  } else if(arr.includes('string')){
-    return 'Array can only contain numbers'
+  if(arr === undefined){
+    throw new Error('Array cannot be undefined')
+  } else if(!Array.isArray(arr)){
+    throw new Error('Argument must be an array')
   } else {
-    return arr.filter((number) => number % 2 === 0);
+    return arr.filter((number) => {
+      if(typeof number !== 'number'){
+        throw new Error('Array can only contain numbers')
+      }
+    
+    return number % 2 === 0});
   }
 
   
