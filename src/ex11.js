8c48b573f66767271939d748e3b9e3cc54105226
Sae Nakatani <nakatanisae0218@gmail.com>
2026-06-12 11:59:56 -0700

    Exercise 11 completed

--- src/ex11.js
+++ src/ex11.js
@@ -6,6 +6,25 @@
  * @returns {Array}
  */
 function rotateArray(arr, positions) {
+  if(arr === undefined){
+    throw new Error('Array cannot be undefined')
+  } 
+  else if(positions === undefined){
+    throw new Error('Positions cannot be undefined')
+  }
+  else if(!Array.isArray(arr)){
+    throw new Error('Argument must be an array')
+  }
+  else if(typeof positions !== 'number'){
+    throw new Error('Positions must be a number')
+  }
+  else if(!Number.isInteger(positions)){
+    throw new Error('Positions must be an integer')
+  }
+  else if(arr.length === 0){
+    return [];
+  }
+
   const splitIndex = arr.length - positions;
   return [...arr.slice(splitIndex), ...arr.slice(0, splitIndex)];
 }
--- src/ex12.js
+++ src/ex12.js
@@ -5,9 +5,24 @@
  * @returns {{characters: number, words: number, sentences: number}}
  */
 function analyzeText(str) {
+  if(str === undefined){
+    throw new Error('String cannot be undefined')
+  }
+  
+  if(typeof str !== 'string'){
+    throw new Error('Argument must be a string')
+  }
+
+  if(str.trim() === ''){
+    return { characters: str.length, words: 0, sentences: 0 };
+  }
+  
+
+  //for
+
   return {
     characters: str.length,
-    words: str.split(' ').length,
+    words: str.trim().split(' ').length,
     sentences: str.split('.').length - 1,
   };
 }
